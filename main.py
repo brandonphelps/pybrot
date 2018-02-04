1da494e64061c902b4bb258f9356d02f3b64987b
brandon phelps <bwp44f@mst.edu>
2018-02-04 15:26:56 -0600

    nother typeo

--- main.py
+++ main.py
@@ -85,7 +85,7 @@ def main():
     count = 1000
     zoom = zoom_box(-1.65, 0, .05, .8)
     print("Zoom box: {}".format(zoom))
-    for j in tqdm(gen_grid(count, zoom[0], zoom[1])), total=(count*count)):
+    for j in tqdm(gen_grid(count, zoom[0], zoom[1]), total=(count*count)):
         grid.append(find_iter(ComplexNumber(*j)))
 
     print(func_z.cache_info())
