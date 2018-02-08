b6d3c81eb98785d3a96a7d73b342997d2c9a1329
brandon <bwp44f@mst.edu>
2018-02-07 20:56:41 -0600

    changed which ip

--- mandelbrot.py
+++ mandelbrot.py
@@ -2,12 +2,10 @@
 from celery import Celery
 import getpass
 
-# , backend='redis://192.168.1.200:6370')
-
 t = '192.168.1.200'
 
-app = Celery('mandel', broker='redis://127.0.0.1:6379/0',
-             backend='redis://127.0.0.1:6379/0')
+app = Celery('mandel', broker='redis://192.168.1.200:6379/0',
+             backend='redis://192.168.1.200:6379/0')
 
 @app.task
 def func_z(n, c):
